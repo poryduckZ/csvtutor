b498e2ec567d4b01483eca08e36a75fdcfec07c0
Goh Yong Jie <gohyongjie1112@gmail.com>
2024-04-09 22:55:11 +0800

    feat: add file path argument instead of stdin

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 extern crate csv;
 
-use std::{error::Error, io, process};
+use std::{env, error::Error, ffi::OsString, fs::File, process};
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
 
 fn main() {
     if let Err(err) = run() {
         println!("{}", err);
         process::exit(1);
     }
-}
-
-fn run() -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_reader(io::stdin());
-    for result in reader.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
 }
